707306cf499a5c7171948efd1c12a550c31771be
xXminimannenXx <minimannen06@gmail.com>
2025-12-22 15:54:52 +0100

    hundklassv1Klar

--- HundRegistret/src/Dog.java
+++ HundRegistret/src/Dog.java
@@ -1,15 +1,91 @@
+
+import java.util.List;
+
 public class Dog {
-    public final double TAX_TAIL_LENGHT = 3.7;
+    public static final double TAX_TAIL_LENGHT = 3.7;
     String name;
     String breed;
     int age;
     int weight;
-   
+    static final List<String> TAX_OLIKA_SPRÅK = List.of(
+    "bassê",
+    "bassotto",
+    "broc_chú",
+    "brochgi",
+    "broghki",
+    "cão-salsicha",
+    "coo brock",
+    "dachs",
+    "dachshound",
+    "dachshund",
+    "dachshunde",
+    "dackel",
+    "dafadog",
+    "dakel",
+    "dakhund",
+    "daks",
+    "dakshund",
+    "dakshunt",
+    "dakszli",
+    "dashhound",
+    "dashond",
+    "gravhund",
+    "gravhundur",
+    "grevlingshund",
+    "jamnik",
+    "jazbečar",
+    "jazvečík",
+    "jezevčík",
+    "jôpscownik",
+    "kolbasohundo",
+    "mäyräkoira",
+    "melhundo",
+    "ofi' falaa'",
+    "omnsrørhund",
+    "omnsrøyrhund",
+    "ovnsrørhund",
+    "perro salchicha",
+    "pølsehund",
+    "salsichinha",
+    "tacskó",
+    "taksas",
+    "taksis",
+    "tax",
+    "teckel",
+    "wiener_dog",
+    "ντάκσχουντ",
+    "да́кел",
+    "јазавичар",
+    "такса",
+    "та́кса",
+    "ტაქસા",
+    "דאַכסהונט טאַקסע",
+    "דקל",
+    "דָּקֶל",
+    "תחש",
+    "תַּחַשׁ",
+    "ഡാഷ്ഹണ്ട്",
+    "แด็กซันด์",
+    "닥스훈트",
+    "ダックスフント",
+    "腊肠犬",
+    "臘腸犬",
+    "达克斯猎犬",
+    "達克斯獵犬"
+);
 
-    public Dog(String name, String breed, int age, int weight, double tailLenght){
 
-        this.name = name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase();
-        this.breed = breed.substring(0,1).toUpperCase() + breed.substring(1).toLowerCase();
+    public Dog(String name, String breed, int age, int weight){
+        //göra en tax check på olika språk sedan standarisera inputen
+        breed = breed.trim();
+        breed = breed.toLowerCase();
+        if(TAX_OLIKA_SPRÅK.contains(breed))breed = "Tax";
+        name = name.trim();
+        this.name = cap(name);
+        this.breed = cap(breed);
+        this.age = age;
+        this.weight = weight;
+        
     }
     public String getName(){
         return name;
@@ -24,14 +100,20 @@ public class Dog {
     public int getWeight(){
         return weight;
     }
-    public double getTailLenght(){ //TODO: fixa språk/standarisera de #supportaAllaSpråk
+    public double getTailLenght(){ 
         
-        if(breed != "Tax")return (age*weight)/10;
+        if(!"Tax".equals(breed))return (age*weight)/10; 
         else return TAX_TAIL_LENGHT;
         
     }
     @Override
     public String toString(){
-        return name + breed + age + weight + getTailLenght();
+        return name + " " + breed + " " + age  + " " + weight + " " + getTailLenght();
+    }
+    private String cap(String s){
+        if(s.isEmpty()) return s;
+        else{
+            return s.substring(0,1).toUpperCase() + s.substring(1).toLowerCase();
+        }
     }
 }
