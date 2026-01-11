fe8a00365bf3d85158e5623e4542cb14285dbcd8
xXminimannenXx <minimannen06@gmail.com>
2026-01-11 23:23:44 +0100

    början på 11

--- HundRegistret/src/OwnerCollection.java
+++ HundRegistret/src/OwnerCollection.java
@@ -1,5 +1,35 @@
 import java.util.ArrayList;
 
 public class OwnerCollection {
+    ArrayList<Owner> currentOwners = new ArrayList<Owner>();
+    public boolean addOwner(Owner newOwner){
+        if(newOwner != null){
+        if(!containsOwner(newOwner)){
+        currentOwners.add(newOwner);
+        return true;
+        }
+        }
+        return false;
+    }
+    public boolean removeOwner(Owner owner){
+        if(owner != null && containsOwner(owner)){
+            
+            currentOwners.set(currentOwners.indexOf(owner), null);
+            return true;
+        }
+        return false;
+    }
     
+    public boolean containsOwner(String ownerName){
+        for(Owner o : currentOwners){
+            if(o.getName().equalsIgnoreCase(ownerName)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean containsOwner(Owner owner){
+        if(containsOwner(owner.getName())) return true;
+        return false;
+    }
 }
